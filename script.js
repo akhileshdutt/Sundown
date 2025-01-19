9115fe6edbc5404df619116c526f9ebe285e838d
akhileshdutt <akhilesh.05.dutt@gmail.com>
2025-01-20 01:22:33 +0530

    day_6

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-import LocomotiveScroll from 'locomotive-scroll';
+// import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+var a = document.querySelector("#elem1")
+var b = a.getAttribute("data-img")
+console.log(b)
